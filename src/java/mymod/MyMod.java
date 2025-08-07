bc5beed8dbf889ecfbf1929f70482c179d643dd2
128_OTEML <3693005811@qq.com>
2025-08-07 11:36:45 +0800

    复制厄兆

--- src/java/mymod/MyMod.java
+++ src/java/mymod/MyMod.java
@@ -1,1 +1,17 @@
 
+package java.mymod;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class MyMod extends Mod {
+    public MyMod() {
+        Log.info("MyMod loaded.");
+    }
+
+    @Override
+    public void loadContent() {
+        // 无需手写注册，hjson 会自动被加载
+        Log.info("Content loaded.");
+    }
+}
